5c9b863f3e00f18c868aeef5d193ff7e4f5150f9
周永乐 <zhouyongle@fenxiangbuy.com>
2020-09-18 10:12:31 +0800

    TimeUtils 增加得到指定月的天数、取得当月天数方法

--- src/main/java/com/imyyq/mvvm/utils/TimeUtil.kt
+++ src/main/java/com/imyyq/mvvm/utils/TimeUtil.kt
@@ -1961,6 +1961,28 @@ class TimeUtils private constructor() {
         }
     }
 
+    /**
+     * 取得当月天数
+     */
+    fun getCurrentMonthLastDay(): Int {
+        val a = Calendar.getInstance()
+        a[Calendar.DATE] = 1 //把日期设置为当月第一天
+        a.roll(Calendar.DATE, -1) //日期回滚一天，也就是最后一天
+        return a[Calendar.DATE]
+    }
+
+    /**
+     * 得到指定月的天数
+     */
+    fun getMonthLastDay(year: Int, month: Int): Int {
+        val a = Calendar.getInstance()
+        a[Calendar.YEAR] = year
+        a[Calendar.MONTH] = month - 1
+        a[Calendar.DATE] = 1 //把日期设置为当月第一天
+        a.roll(Calendar.DATE, -1) //日期回滚一天，也就是最后一天
+        return a[Calendar.DATE]
+    }
+
     init {
         throw UnsupportedOperationException("u can't instantiate me...")
     }
